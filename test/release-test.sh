038ae6b842b301010f1b047cc8b27ada7e2c7788
Daniel Krasinski <daniel.krasinski@oracle.com>
2024-10-18 15:22:46 +0000

    Add setup for oci cluster api provider

---
+++ test/release-test.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+
+# Make sure all relevant environment variables are set
+if [ -z "$CAPI_MANAGEMENT_CLUSTER_DEFINITION" ]; then
+	echo 'The CAPI_MANAGEMENT_CLUSTER_DEFINITION environment variable must point to a cluster definition file'
+	exit 1
+fi
+
+if [ ! -f "$CAPI_MANAGEMENT_CLUSTER_DEFINITION" ]; then
+	echo "The CAPI_MANAGEMENT_CLUSTER_DEFINITION file, $CAPI_MANAGEMENT_CLUSTER_DEFINITION, does not exist"
+	exit 1
+fi
+
+CAPI_MANAGEMENT_CLUSTER_DEFINITION=$(realpath "$CAPI_MANAGEMENT_CLUSTER_DEFINITION")
+
+# Run the complete test suite
+./run_tests.sh '' 1 1
