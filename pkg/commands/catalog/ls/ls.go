c2c49916c04d2c9be8ea01d3ca325d296a509701
raaghav wadhawan <raaghav.w.wadhawan@oracle.com>
2024-10-28 16:35:32 -0700

    catalog ls to find local catalogs

--- pkg/commands/catalog/ls/ls.go
+++ pkg/commands/catalog/ls/ls.go
@@ -4,11 +4,14 @@
 package ls
 
 import (
-	"k8s.io/apimachinery/pkg/types"
+	"fmt"
 	"github.com/oracle-cne/ocne/pkg/catalog"
 	"github.com/oracle-cne/ocne/pkg/constants"
 	"github.com/oracle-cne/ocne/pkg/k8s"
 	"github.com/oracle-cne/ocne/pkg/k8s/client"
+	"k8s.io/apimachinery/pkg/types"
+	"os"
+	"path/filepath"
 )
 
 // Ls gets a logs of catalogs
@@ -51,5 +54,41 @@ func Ls(kubeconfig string) ([]catalog.CatalogInfo, error) {
 			Type:        service.Spec.Type,
 		})
 	}
+	// Add local catalogs from ~/.ocne/catalogs
+	homeDir, err := os.UserHomeDir()
+	if err != nil {
+		return nil, fmt.Errorf("unable to access home directory: %v", err)
+	}
+
+	localCatalogPath := filepath.Join(homeDir, ".ocne", "catalogs")
+	files, err := os.ReadDir(localCatalogPath)
+	if err != nil {
+		return nil, fmt.Errorf("unable to read local catalogs: %v", err)
+	}
+
+	// Parse each file as a local catalog
+	for _, file := range files {
+		if file.IsDir() {
+			continue
+		}
+
+		filePath := filepath.Join(localCatalogPath, file.Name())
+		content, err := os.ReadFile(filePath)
+		if err != nil {
+			fmt.Printf("Failed to read catalog file %s: %v\n", filePath, err)
+			continue
+		}
+
+		var localCatalog catalog.CatalogInfo
+		if err := yaml.Unmarshal(content, &localCatalog); err != nil {
+			fmt.Printf("Failed to parse catalog file %s: %v\n", filePath, err)
+			continue
+		}
+
+		// Add local catalog to the list
+		catalogs = append(catalogs, localCatalog)
+	}
+
 	return catalogs, nil
+
 }
