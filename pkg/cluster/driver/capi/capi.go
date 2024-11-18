d993aef890867a86927aff9a862fbd2eea9cdd47
Zaid Abdulrehman <zaid.a.abdulrehman@oracle.com>
2024-11-18 11:28:34 -0800

    Remove comment

--- pkg/cluster/driver/capi/capi.go
+++ pkg/cluster/driver/capi/capi.go
@@ -1057,7 +1057,5 @@ func (cad *ClusterApiDriver) PostInstallHelpStanza() string {
 }
 
 func (Cad *ClusterApiDriver) DefaultCNIInterfaces() []string {
-	// ens3 is the default OCI vNIC name for x86
-	// enp0s6 is the default for arm
 	return []string{}
 }
