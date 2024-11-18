c78c73921c53697f0b3a881a90eab1b9acf17d5b
Zaid Abdulrehman <zaid.a.abdulrehman@oracle.com>
2024-11-18 11:38:04 -0800

    Remove formatting changes

--- pkg/cluster/driver/capi/capi.go
+++ pkg/cluster/driver/capi/capi.go
@@ -70,8 +70,8 @@ func (cad *ClusterApiDriver) getApplications() ([]install.ApplicationDescription
 
 	proxyValues := map[string]interface{}{
 		"httpsProxy": cad.ClusterConfig.Providers.Oci.Proxy.HttpsProxy,
-		"httpProxy":  cad.ClusterConfig.Providers.Oci.Proxy.HttpProxy,
-		"noProxy":    cad.ClusterConfig.Providers.Oci.Proxy.NoProxy,
+		"httpProxy": cad.ClusterConfig.Providers.Oci.Proxy.HttpProxy,
+		"noProxy": cad.ClusterConfig.Providers.Oci.Proxy.NoProxy,
 	}
 
 	return []install.ApplicationDescription{
@@ -938,7 +938,7 @@ func (cad *ClusterApiDriver) waitForClusterDeletion(clusterName string, clusterN
 		} else {
 			log.Debugf("Resource for cluster %s/%s was nil", clusterNs, clusterName)
 		}
-		if err != nil {
+		if err != nil{
 			if strings.Contains(err.Error(), "not found") {
 				return nil, false, nil
 			}
@@ -967,7 +967,7 @@ func (cad *ClusterApiDriver) deleteCluster(clusterName string, clusterNs string)
 	haveError := logutils.WaitFor(logutils.Info, []*logutils.Waiter{
 		{
 			Message: "Waiting for deletion",
-			WaitFunction: func(i interface{}) error {
+			WaitFunction: func(i interface{}) error{
 				return cad.waitForClusterDeletion(clusterName, clusterNs)
 			},
 		},
