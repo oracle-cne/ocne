0ed622ff5250fb3ac96660fb26cf2368ce5596ed
George Aeillo <george.f.aeillo@oracle.com>
2024-11-26 16:42:08 -0500

    Further increase timeout

--- pkg/commands/catalog/mirror/mirror.go
+++ pkg/commands/catalog/mirror/mirror.go
@@ -123,7 +123,7 @@ func Mirror(options Options) error {
 				return err
 			}
 			log.Debugf("Copying %s:%s to system", imageInfo.BaseImage, imageInfo.Tag)
-			time.Sleep(15 * time.Second)
+			time.Sleep(30 * time.Second)
 			err = imageUtil.Copy(fmt.Sprintf("docker://%s", image), "oci-archive:"+ociArchiveDir+"/"+strconv.Itoa(counter)+".oci:"+imageInfo.BaseImage+":"+imageInfo.Tag, "", copy.CopyAllImages)
 			if err != nil {
 				return err
