14bda3bb1ce1c8dee11f4c93414bf17213e9f558
pfmackin <paul.mackin@oracle.com>
2025-03-18 19:04:06 -0400

    update olvm template for new OCK image

--- pkg/cluster/template/olvm/ignition.go
+++ pkg/cluster/template/olvm/ignition.go
@@ -91,6 +91,11 @@ func getExtraIgnition(config *types.Config, clusterConfig *types.ClusterConfig, 
 		return "", err
 	}
 
+	patches, err := ignition.KubeadmPatches()
+	if err != nil {
+		return "", err
+	}
+
 	ign := ignition.NewIgnition()
 
 	// Cluster API has its own kubeadm service to start
@@ -164,6 +169,7 @@ func getExtraIgnition(config *types.Config, clusterConfig *types.ClusterConfig, 
 	ign = ignition.Merge(ign, container)
 	ign = ignition.Merge(ign, proxy)
 	ign = ignition.Merge(ign, usr)
+	ign = ignition.Merge(ign, patches)
 
 	// If an internal LB is needed for the control plane then the kubeconfig file
 	// needs to be copied to /etc/keepalived
