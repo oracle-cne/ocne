4e1973103daf07e46cea42cbb123e2ec7cc12b36
Michael Gianatassio <michael.gianatassio@oracle.com>
2025-04-25 14:54:20 -0700

    Remove unused code

--- pkg/cluster/driver/olvm/stage.go
+++ pkg/cluster/driver/olvm/stage.go
@@ -4,25 +4,16 @@
 package olvm
 
 import (
-	"context"
 	"fmt"
 	"os"
 	"strings"
 
-	"github.com/oracle-cne/ocne/pkg/k8s"
-
-	"github.com/oracle-cne/ocne/pkg/util"
-
-	"github.com/containers/image/v5/transports/alltransports"
 	"github.com/oracle-cne/ocne/pkg/catalog/versions"
 	"github.com/oracle-cne/ocne/pkg/cluster/template/common"
-	"github.com/oracle-cne/ocne/pkg/cmdutil"
-	"github.com/oracle-cne/ocne/pkg/constants"
-	"github.com/oracle-cne/ocne/pkg/image"
+	"github.com/oracle-cne/ocne/pkg/k8s"
 	"github.com/oracle-cne/ocne/pkg/k8s/client"
+	"github.com/oracle-cne/ocne/pkg/util"
 	"github.com/oracle-cne/ocne/pkg/util/capi"
-	"github.com/oracle-cne/ocne/pkg/util/oci"
-	"github.com/oracle/oci-go-sdk/v65/core"
 	log "github.com/sirupsen/logrus"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 )
@@ -96,82 +87,17 @@ func (cad *OlvmDriver) Stage(version string) (string, string, bool, error) {
 	log.Debugf("kubernetes minor version changes = %t", minorVersionChanged)
 
 	// Get the collection of vmTemplateNames in use
-	ociImages, err := graphToVMTemplates(graph)
+	vmTemplates, err := graphToVMTemplates(graph)
 	if err != nil {
 		return "", "", false, err
 	}
-	log.Debugf("%v", ociImages)
+	log.Debugf("%v", vmTemplates)
 
-	/*
-		err = findUpdates(ociImages, version, cad.ClusterConfig.BootVolumeContainerImage, cad.ClusterConfig.Providers.Oci.Profile)
-		if err != nil {
-			return "", "", false, err
-		}
-
-		imageImports := map[string]string{}
-		for id, img := range ociImages {
-			if img.HasUpdate {
-				log.Debugf("OCI image %s with architecture %s has an update", id, img.Arch)
-			} else if minorVersionChanged {
-				log.Debugf("Updating image OCID due to Kubernetes minor version change")
-				// If the minor version has changed, go get the latest
-				// image for the new minor version.
-				existingImg, found, err := oci.GetImage(*img.Image.DisplayName, version, img.Arch, *img.Image.CompartmentId, cad.ClusterConfig.Providers.Oci.Profile)
-				if !found {
-					// In theory this is impossible because the
-					// check to see if a new image must be uploaded
-					// has already found one, but impossible things
-					// happen every day.
-					return "", "", false, fmt.Errorf("Could not find latest OCI image for Kubernetes version %s and architecture %s", version, img.Arch)
-				}
-				if err != nil {
-					return "", "", false, err
-				}
-
-				img.HasUpdate = true
-				img.NewId = *existingImg.Id
-				continue
-			} else {
-				log.Debugf("OCI image %s with architecture %s does not have an update", id, img.Arch)
-				continue
-			}
-
-			oldBv := cad.ClusterConfig.BootVolumeContainerImage
-			imgXport := alltransports.TransportFromImageName(cad.ClusterConfig.BootVolumeContainerImage)
-			if imgXport == nil {
-				cad.ClusterConfig.BootVolumeContainerImage = fmt.Sprintf("docker://%s", cad.ClusterConfig.BootVolumeContainerImage)
-			}
-			cad.ClusterConfig.BootVolumeContainerImage, err = cmdutil.EnsureBootImageVersion(version, cad.ClusterConfig.BootVolumeContainerImage)
-			img.NewId, img.WorkRequestId, err = cad.ensureImage(*img.Image.DisplayName, img.Arch, version, true)
-			cad.ClusterConfig.BootVolumeContainerImage = oldBv
-			if err != nil {
-				return "", "", false, err
-			}
-
-			imageImports[img.WorkRequestId] = fmt.Sprintf("Importing updated image for %s", *img.Image.DisplayName)
-		}
-
-		err = oci.WaitForWorkRequests(imageImports, cad.ClusterConfig.Providers.Oci.Profile)
-		if err != nil {
-			return "", "", false, err
-		}
-
-		for _, img := range ociImages {
-			if img.WorkRequestId != "" {
-				err = upload.EnsureImageDetails(*img.Image.CompartmentId, cad.ClusterConfig.Providers.Oci.Profile, img.NewId, img.Arch)
-
-				if err != nil {
-					return "", "", false, err
-				}
-			}
-		}
-
-	*/
 	// Make the new machine templates by creating a new OLVMMachineTemplate
 	// for each existing one that uses an existing OLVM Template.
 	updatedMts := map[*capi.GraphNode]*unstructured.Unstructured{}
 
-	for _, img := range ociImages {
+	for _, img := range vmTemplates {
 		log.Debugf("Creating template for %s", img.NewTemplate)
 		newId := img.NewTemplate
 
@@ -278,92 +204,6 @@ func imageFromMachineTemplate(mt *unstructured.Unstructured) (string, error) {
 	return templateName, nil
 }
 
-// doUpdate calculates if there is reason to upload a new OCI custom image
-// for a given existing image.
-func doUpdate(img *core.Image, arch string, version string, bvImage string, profile string) (bool, error) {
-	// Update the image if:
-	// - An environment variable forces the update (typically for testing)
-	// - The minor version is changing and an image for that version
-	//   does not exist
-	// - The minor version is not changing and the container image is
-	//   newer than the OCI image
-	//
-	// An image can be forcibly created and uploaded to make it easier
-	// to test the upload process.  This logic is reliant on existing
-	// resources and timestamps that are difficult to control.  Instead of
-	// forcing testers to design complex harnesses, this environment
-	// variable makes it easy to simulate an available update.
-	if os.Getenv("OCNE_OCI_STAGE_FORCE_UPLOAD") != "" {
-		return true, nil
-	}
-
-	imgKubeVersion, ok := img.FreeformTags[constants.OCIKubernetesVersionTag]
-	if !ok {
-		return false, fmt.Errorf("OCI Custom image %s does not have a Kubernetes version tag", *img.Id)
-	}
-
-	imgName := *img.DisplayName
-	compartmentId := *img.CompartmentId
-
-	existingImg, found, err := oci.GetImage(imgName, version, arch, compartmentId, profile)
-	if err != nil {
-		return false, err
-	}
-	if found {
-		log.Debugf("Found existing OCI Image for version %s with OCID %s", version, *existingImg.Id)
-	}
-
-	// If the versions are the same, check for newer OCI images first
-	kubeCmp, err := versions.CompareKubernetesVersions(imgKubeVersion, version)
-	if err != nil {
-		return false, err
-	} else if kubeCmp == 0 {
-		// GetImage returns the newest image with the same version and
-		// arch.  If the OCIDs of the image from the template and the
-		// newest image are the same, then it must be the latest.
-		// Otherwise it must not.
-		if *existingImg.Id == *img.Id {
-			// Check for a new image
-
-			containerImg, err := cmdutil.EnsureBootImageVersion(version, bvImage)
-			if err != nil {
-				return false, nil
-			}
-
-			imgXport := alltransports.TransportFromImageName(containerImg)
-			if imgXport == nil {
-				containerImg = fmt.Sprintf("docker://%s", containerImg)
-			}
-
-			ockImgSpec, err := image.GetImageSpec(containerImg, arch)
-			if err != nil {
-				return false, err
-			}
-			log.Debugf("Have container image spec for %s", containerImg)
-
-			ockImgInfo, err := ockImgSpec.Inspect(context.Background())
-			if err != nil {
-				return false, err
-			}
-			log.Debugf("Inspecting container image info for %s", containerImg)
-
-			log.Debugf("Checking %v against %v", ockImgInfo.Created, existingImg.TimeCreated.Time)
-			if ockImgInfo.Created.After(existingImg.TimeCreated.Time) {
-				// Upload the new image
-				return true, nil
-			}
-		}
-	} else if found {
-		// Don't upload the new image
-		return false, nil
-	} else {
-		// Upload the new image
-		return true, nil
-	}
-
-	return false, nil
-}
-
 // graphToVMTemplates scrapes the graph of CAPI resources and extracts the
 // template names from the OLVMMachineTemplates.
 func graphToVMTemplates(graph *capi.ClusterGraph) (map[string]*TemplateData, error) {
@@ -392,17 +232,3 @@ func graphToVMTemplates(graph *capi.ClusterGraph) (map[string]*TemplateData, err
 	}, ret)
 	return ret, err
 }
-
-// findUpdates checks to see if an update is available for a set of images.
-/*
-func findUpdates(imgs map[string]*TemplateData, version string, bvImage string, profile string) error {
-	for _, img := range imgs {
-		var err error
-		img.HasUpdate, err = doUpdate(img.Image, img.Arch, version, bvImage, profile)
-		if err != nil {
-			return err
-		}
-	}
-	return nil
-}
-*/
