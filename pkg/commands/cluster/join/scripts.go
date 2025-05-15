5e04454cbe28190e5131be8c6a7e86129b41d8f0
Daniel Krasinski <daniel.krasinski@oracle.com>
2025-05-15 15:37:31 -0400

    Fix various issues with cluster join (#420)

--- pkg/commands/cluster/join/scripts.go
+++ pkg/commands/cluster/join/scripts.go
@@ -13,8 +13,14 @@ chroot /hostroot mv /etc/kubernetes/kubeadm.conf /etc/kubernetes/kubeadm.conf.ba
 chroot /hostroot bash -c "echo \"$JOIN_CONFIG\" > /etc/kubernetes/kubeadm.conf"
 chroot /hostroot sed -i '/Environment=ACTION=/c\Environment=ACTION=join' /etc/systemd/system/ocne.service.d/bootstrap.conf
 chroot /hostroot systemctl daemon-reload
+chroot /hostroot chown keepalived_script:keepalived_script /etc/keepalived/peers /etc/keepalived/keepalived.conf /etc/keepalived/log /etc/ocne/nginx/servers /etc/ocne/nginx/servers /etc/ocne/nginx/nginx.conf || true
 chroot /hostroot bash -c 'for svc in $ENABLE_SERVICES; do systemctl enable $svc --now; done'
 chroot /hostroot touch /etc/ocne/reset-kubeadm
 chroot /hostroot systemctl restart ocne
+`
+
+	// Get the network interface that hosts the route for an IP address
+	defaultRouteScript = `#! /bin/bash
+ip -o route get %s | cut -d' ' -f3
 `
 )
