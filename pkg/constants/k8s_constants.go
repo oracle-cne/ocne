0cbce16ede5b6512a602b30a27fe0e78ab89e73c
Michael Gianatassio <michael.gianatassio@oracle.com>
2025-06-26 16:57:27 -0400

    Default to installing latest CAPI components

--- pkg/constants/k8s_constants.go
+++ pkg/constants/k8s_constants.go
@@ -104,19 +104,19 @@ const (
 	CoreCAPIRelease    = "core-capi"
 	CoreCAPINamespace  = "capi-system"
 	CoreCAPIChart      = "core-capi"
-	CoreCAPIVersion    = "1.9.9"
+	CoreCAPIVersion    = ""
 	CoreCAPIDeployment = "core-capi-controller-manager"
 
 	KubeadmBootstrapCAPIRelease    = "bootstrap-capi"
 	KubeadmBootstrapCAPINamespace  = "capi-kubeadm-bootstrap-system"
 	KubeadmBootstrapCAPIChart      = "bootstrap-capi"
-	KubeadmBootstrapCAPIVersion    = "1.9.9"
+	KubeadmBootstrapCAPIVersion    = ""
 	KubeadmBootstrapCAPIDeployment = "bootstrap-capi-controller-manager"
 
 	KubeadmControlPlaneCAPIRelease    = "control-plane-capi"
 	KubeadmControlPlaneCAPINamespace  = "capi-kubeadm-control-plane-system"
 	KubeadmControlPlaneCAPIChart      = "control-plane-capi"
-	KubeadmControlPlaneCAPIVersion    = "1.9.9"
+	KubeadmControlPlaneCAPIVersion    = ""
 	KubeadmControlPlaneCAPIDeployment = "control-plane-capi-controller-manager"
 
 	OCICAPIRelease    = "capoci"
