a54fe160daedaa2872e93b651cdc04a3d0dd563d
Daniel Krasinski <daniel.krasinski@oracle.com>
2025-11-17 22:12:21 +0000

    Add on-host checks for updatability for Kubernetes 1.32.7

--- pkg/k8s/node.go
+++ pkg/k8s/node.go
@@ -208,16 +208,6 @@ func IsControlPlane(node *v1.Node) bool {
 	cp, _ := GetRole(node)
 	return cp
 }
-
-// IsUpdateAvailable returns true if an update is available on that node
-func IsUpdateAvailable(node *v1.Node) bool {
-	update := false
-	if node.ObjectMeta.Annotations != nil && node.ObjectMeta.Annotations[constants.OCNEAnnoUpdateAvailable] == "true" {
-		update = true
-	}
-	return update
-}
-
 func processImage(registry string, best string, secondBest string, img *v1.ContainerImage) (string, bool, bool) {
 	haveBest := false
 	exactMatch := false
