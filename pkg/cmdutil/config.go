c40775c99ce6ba0723ed831c2a96b02bff688989
Daniel Krasinski <daniel.krasinski@oracle.com>
2025-11-19 19:21:48 +0000

    Update EnsureBootImageVersion to allow for replacing standard tags on container image references

--- pkg/cmdutil/config.go
+++ pkg/cmdutil/config.go
@@ -4,8 +4,10 @@
 package cmdutil
 
 import (
+	"fmt"
 	"strings"
 
+	"github.com/oracle-cne/ocne/pkg/catalog/versions"
 	"github.com/oracle-cne/ocne/pkg/config"
 	"github.com/oracle-cne/ocne/pkg/config/types"
 	"github.com/oracle-cne/ocne/pkg/image"
@@ -48,6 +50,43 @@ func EnsureBootImageVersion(kubeVersion string, imageForContainer string) (strin
 	if err != nil {
 		return imageForContainer, err
 	}
+
+	// Since the early days, the cluster cache has stored the post-processed
+	// value for BootVolumeContainerImage.  That is, the value that is
+	// returned by this function.  The value will always contain a tag.
+	// Often, like during an update, the tag has to change.  But sometimes
+	// it does not, such as when a non-standard tag or sha is specified.
+	// That creates a bind where the tag needs to be changed but it's not
+	// possible to tell if a specific tag was configured or the tag came
+	// from this functions auto-tagging facility.  To handle both cases,
+	// see if the tag is a typical OCK tag (read: <major>.<minor>).  If it
+	// is, then strip the tag.  Otherwise, preserve the tag.  That will
+	// allow updates to the tag when appropriate without destroying the
+	// ability to specify a custom tag/digest.  There is one corner case.
+	// A cluster configuration may have explicity configured a standard
+	// tag.  This logic will strip that tag in leiu of a new one.  The
+	// problem only applies to times when Kubernetes is being updated to
+	// another minor version.  It is diffult to imagine that happening in
+	// the real world.  At some point you've gone out of your way to make
+	// your own life hard...
+	if imgInfo.Tag != "" {
+		ok, err := versions.IsSupportedKubernetesVersion(imgInfo.Tag)
+		if err != nil {
+			return "", err
+		}
+		if ok {
+			imgInfo.Tag = ""
+
+			components := strings.Split(imageForContainer, ":")
+			if len(components) < 2 {
+				return "", fmt.Errorf("invalid image %s", imageForContainer)
+			}
+
+			imageForContainer = strings.Join(components[:len(components)-1], ":")
+		}
+	}
+
+
 	if imgInfo.Tag == "" && imgInfo.Digest == "" {
 		// if the version contains a "v" prefix, strip it
 		ver := strings.TrimPrefix(kubeVersion, "v")
