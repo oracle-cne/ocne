165835309ffe829f46e58022ec73ea0827e0b6d1
Daniel Krasinski <daniel.krasinski@oracle.com>
2025-12-15 20:42:11 +0000

    Choose between ClusterConfiguration versions

--- pkg/k8s/clusterconfiguration.go
+++ pkg/k8s/clusterconfiguration.go
@@ -1,41 +1,54 @@
 package k8s
 
 import (
-    "fmt"
+	"fmt"
 
-    "k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/kubernetes/cmd/kubeadm/app/apis/kubeadm"
+	kubeadmv1beta4 "k8s.io/kubernetes/cmd/kubeadm/app/apis/kubeadm/v1beta4"
+	kubeadmv1beta3 "k8s.io/kubernetes/cmd/kubeadm/app/apis/kubeadm/v1beta3"
+	"k8s.io/apimachinery/pkg/runtime/serializer/json"
+	"k8s.io/apimachinery/pkg/runtime"
 
-    kubeadmv1beta4 "k8s.io/kubernetes/cmd/kubeadm/app/apis/kubeadm/v1beta4"
-    "k8s.io/apimachinery/pkg/runtime/serializer/json"
-    "k8s.io/apimachinery/pkg/runtime"
-
-    "github.com/oracle-cne/ocne/pkg/constants"
+	"github.com/oracle-cne/ocne/pkg/constants"
 )
 
-func GetKubeadmClusterConfiguration(client kubernetes.Interface) (*kubeadmv1beta4.ClusterConfiguration, error) {
-    cm, err := GetConfigmap(client, constants.KubeNamespace, constants.KubeCMName)
-    if err != nil {
-	    return nil, err
-    }
+func GetKubeadmClusterConfiguration(client kubernetes.Interface) (*kubeadm.ClusterConfiguration, error) {
+	cm, err := GetConfigmap(client, constants.KubeNamespace, constants.KubeCMName)
+	if err != nil {
+		return nil, err
+	}
 
-    clusterConfigYAML, exists := cm.Data[constants.KubeCMField]
-    if !exists {
-        return nil, fmt.Errorf("ClusterConfiguration not found in kubeadm-config ConfigMap")
-    }
+	clusterConfigYAML, exists := cm.Data[constants.KubeCMField]
+	if !exists {
+		return nil, fmt.Errorf("%s not found in %s ConfigMap", constants.KubeCMField, constants.KubeCMName)
+	}
 
-    // Decode the YAML into ClusterConfiguration struct
-    scheme := runtime.NewScheme()
-    kubeadmv1beta4.AddToScheme(scheme)
-    decoder := json.NewYAMLSerializer(json.DefaultMetaFactory, scheme, scheme)
-    obj, _, err := decoder.Decode([]byte(clusterConfigYAML), nil, &kubeadmv1beta4.ClusterConfiguration{})
-    if err != nil {
-        return nil, fmt.Errorf("failed to decode ClusterConfiguration: %v", err)
-    }
+	// Decode the YAML into ClusterConfiguration struct
+	scheme := runtime.NewScheme()
+	kubeadmv1beta4.AddToScheme(scheme)
+	kubeadmv1beta3.AddToScheme(scheme)
+	decoder := json.NewYAMLSerializer(json.DefaultMetaFactory, scheme, scheme)
+	obj, _, err := decoder.Decode([]byte(clusterConfigYAML), nil, &kubeadmv1beta4.ClusterConfiguration{})
+	if err != nil {
+		return nil, fmt.Errorf("failed to decode ClusterConfiguration: %v", err)
+	}
 
-    clusterConfig, ok := obj.(*kubeadmv1beta4.ClusterConfiguration)
-    if !ok {
-        return nil, fmt.Errorf("unexpected type after decoding")
-    }
+	ret := kubeadm.ClusterConfiguration{}
+	switch obj.(type) {
+	case *kubeadmv1beta4.ClusterConfiguration:
+		err = kubeadmv1beta4.Convert_v1beta4_ClusterConfiguration_To_kubeadm_ClusterConfiguration(obj.(*kubeadmv1beta4.ClusterConfiguration), &ret, nil)
+		if err != nil {
+			return nil, err
+		}
+	case *kubeadmv1beta3.ClusterConfiguration:
+		err = kubeadmv1beta3.Convert_v1beta3_ClusterConfiguration_To_kubeadm_ClusterConfiguration(obj.(*kubeadmv1beta3.ClusterConfiguration), &ret, nil)
+		if err != nil {
+			return nil, err
+		}
+	default:
+		return nil, fmt.Errorf("ClusterConfiguration had unexpected type")
+	}
 
-    return clusterConfig, nil
+	return &ret, nil
 }
